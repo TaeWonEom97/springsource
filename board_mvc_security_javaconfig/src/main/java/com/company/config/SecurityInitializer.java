c7d1f1076521d00d513523e9738d17807d23fcf6
eomtaewon <utw1960@naver.com>
2021-12-16 18:11:20 +0900

    2021.12.16 security + security config

---
+++ board_mvc_security_javaconfig/src/main/java/com/company/config/SecurityInitializer.java
@@ -0,0 +1,9 @@
+package com.company.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+
+// DelegatingfilterProxy
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
